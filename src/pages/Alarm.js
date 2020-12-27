cbe2f382c756b3a39dd0bd757a0bb3c01b0d864a
M. Mirmousavi <mirmousavi.m@gmail.com>
2020-12-27 21:00:31 +0330

    Add Alarm page

--- src/pages/Alarm.js
+++ src/pages/Alarm.js
@@ -1,6 +1,25 @@
+import React from "react";
+import moment from "moment";
+
 function Alarm() {
+  const [hour, setHour] = React.useState(0);
+  const [minute, setMinute] = React.useState(0);
+
+  React.useEffect(() => {
+    let defaultAlarmTime = moment().add(10, 'minute');
+    setHour(Number(defaultAlarmTime.format("H")));
+    setMinute(Number(defaultAlarmTime.format("m")));
+
+  }, []);
+
+
   return (
-    <p>Alarm</p>
+    <span>
+      <h2>Alarm</h2>
+      <input value={hour} type="number" min={0} max={23}/>
+      <input value={minute} type="number" min={0} max={59}/>
+      <button>Set Alarm</button>
+    </span>
   );
 }
 
