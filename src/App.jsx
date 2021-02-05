e14dede4c7f1b24bc50c2ca4d1cf9e3c7fbbbc66
M. Mirmousavi <mirmousavi.m@gmail.com>
2021-02-06 01:55:35 +0330

    Fix eslint on App.js

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { HashRouter as Router } from 'react-router-dom';
+import DefaultLayout from './layouts/default-layout';
+import { TimezoneProvider } from './context/timezone';
+import { ThemeProvider } from './context/theme';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <TimezoneProvider>
+        <Router>
+          <DefaultLayout />
+          {/*  TODO: Add route for auth pages */}
+        </Router>
+      </TimezoneProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
